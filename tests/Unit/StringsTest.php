cbea9b9050ea94b831cff6f7f9c8b7c1710cb427
Cédric Ducarre <samray1024@gmail.com>
2023-09-12 23:38:27 +0200

    Add errors tests for encryption

--- tests/Unit/StringsTest.php
+++ tests/Unit/StringsTest.php
@@ -17,3 +17,21 @@ test('encryption', function()
 
 	expect(decrypt(encrypt($data, $key), $key))->toBe($data);
 });
+
+test('decryption error', function()
+{
+	$data = 'This string must be encrypted and decrypted';
+	$key = makePrivateKey();
+
+	$encrypted = encrypt($data, $key);
+	$encrypted[random_int(0, strlen($encrypted) - 1)] = 0;
+
+	decrypt($encrypted, $key);
+})
+->throws(UnexpectedValueException::class);
+
+test('encryption cipher error', function()
+{
+	encrypt('example', makePrivateKey(), 'aes-256-abc');
+})
+->throws(LengthException::class);
